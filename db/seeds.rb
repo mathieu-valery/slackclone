ddd3fe7ff19cc2670243a72c23dcb4d959637931
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2022-12-07 13:35:54 +0100

    db ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!([
+    {email: "mathieuromainvalery@gmail.com", password: "123456789", first_name: "Mathieu", last_name: "Valery"},
+    {email: "jg@gmail.com", password: "123456789", first_name: "Jean", last_name: "Guy"},
+    {email: "elonmusk@gmail.com", password: "123456789", first_name: "Elon", last_name: "Musk"},
+    {email: "abitbol@gmail.com", password: "123456789", first_name: "Georges", last_name: "Abitbol"},
+    {email: "mathieuvalery@hotmail.fr", password: "123456789", first_name: nil, last_name: nil}
+  ])
+  Channel.create!([
+    {name: "general"},
+    {name: "paris"},
+    {name: "react"}
+  ])
+  Message.create!([
+    {user_id: 1, channel_id: 1, content: "hello chat"},
+    {user_id: 2, channel_id: 1, content: "hey mate"},
+    {user_id: 2, channel_id: 2, content: "hello paris channel"},
+    {user_id: 3, channel_id: 3, content: "hello react channel"}
+  ])
